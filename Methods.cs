2076dcc211522eb6b2252715479f4526e5c7f077
Vineeth Kakarla <vineethvijay678@gmail.com>
2020-08-23 00:46:01 +0530

    Update Methods.cs

--- Methods.cs
+++ Methods.cs
@@ -1,3 +1,5 @@
+// Methods Topic-1 Initiation and Declaration
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
@@ -7,8 +9,7 @@ using System.Threading.Tasks;
 
 namespace Methods
 {
-   // Methods Topic-1 Initiation and Declaration
-    class MethodDeclaration
+  class MethodDeclaration
     {
         //Declaring Global Variables
         public int A;
@@ -29,10 +30,49 @@ namespace Methods
             Console.WriteLine(X);
             Console.ReadKey();
             
-        }
-        
+        }     
     }
+}
 
+//Topic -2 Method Overloading
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 
- }
+namespace Methods
+{
+    class MethodOverloading
+    {
+        /* Method Overloading is when method has same name and different parameters
+         * This usually occurs when the work is same but changes with parameters
+         */
+
+        // Method is same mechanism but integers as parameters
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        // Method is same mechanism but Doubles as parameters
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+       public static void Main()
+        {
+            int z = Add(12234, 545878);
+            double X = Add(233.44, 434242.45);
+            Console.WriteLine("Integer addition is {0}", z);
+            Console.WriteLine("Double addition is {0}",X);
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
     
